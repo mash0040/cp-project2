aad122066079a39c82b399483abcf9d2645e9970
loukilik84 <loukilik84@gmail.com>
2023-12-04 22:11:01 -0500

    done

--- movies.js
+++ movies.js
@@ -1,10 +1,10 @@
-//TMDB
+// TMDB
 
 const API_KEY = "api_key=1cf50e6248dc270629e802686245c2c8";
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY + "&with_original_language=en"; // Updated line
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
-const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+const searchURL = BASE_URL + "/search/movie?" + API_KEY + "&with_original_language=en"; // Updated line
 
 const genres = [
   {
